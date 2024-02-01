a6e1ed2bfec0409ddab5102837e7dcbd66e061d2
Terry Phillips <terrydphillips@gmail.com>
2024-01-31 21:07:04 -0500

    Update to show column select

--- frc-event-data.py
+++ frc-event-data.py
@@ -5,25 +5,29 @@ import pandas as pd
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
-
-# client.DefaultRequestHeaders.Accept.Clear();
-# client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
+# URL to GET
 api_url = "https://www.thebluealliance.com/api/v3/event/2019ncpem/rankings"
 # api_url = "https://www.thebluealliance.com/api/v3/event/2024ncwak/rankings"
-headers = {'User-Agent': 'Team 5607 Event Data CSV Generator', 'X-TBA-Auth-Key': ''}
+
+# Put your TBA auth key in the last set of quotes
+headers = {'User-Agent': 'Team 5607 Event Data CSV Generator', 'X-TBA-Auth-Key': 'iu9k1Zf1Trq56GHqConIPbpCvzvVlRfzTkfTpi1UAWwuTDtWqbRPkH21kiiK8Hex'}
 response = requests.get(api_url, headers=headers)
+
+# Dumps all of the JSON contents of the GET
 response.json()
 
 reff = pd.json_normalize(response.json()['rankings'])
 
 df = pd.DataFrame(data=reff)
 
+# Select columns from the data frame
+df1 = df[['team_key','record.wins']]
+
 cmap = ListedColormap(['#0343df', '#e50000', '#ffff14', '#929591'])
 
-ax = df.plot.bar(x='team_key', colormap=cmap)
-
-ax.set_xlabel(None)
-ax.set_ylabel('data')
+# Plot 
+ax = df1.plot.bar(x='team_key', colormap=cmap)
+ax.set_xlabel('Team number')
+ax.set_ylabel('Team wins')
 ax.set_title('Team wins')
 plt.show()
